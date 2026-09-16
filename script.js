1791d3a93566398803652cc5cfd00796dc9c6324
ListenBall <y-mikoto-02ynr8@g.bears.ed.jp>
2026-09-16 16:45:01 +0900

    Fix limitquestion display in script.js

--- script.js
+++ script.js
@@ -221,7 +221,7 @@ let templist = [];
 const qwert = ["q", "w", "e", "r", "t"];
 const yuiop = ["y", "u", "i", "o", "p"];
 let limitquestion = 40;
-document.getElementById("limitquestion"); = limitquestion;
+document.getElementById("limitquestion").textContent = limitquestion;
 // 点数計算
 document.addEventListener("keydown", (event) => {
     if (pushdowned == 0 && event.target.tagName != "TD") {
